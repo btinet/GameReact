27b9a43cf6a95c52a2a494bd4347f74d1cf31d9c
Benjamin Wagner <bwagner@vapita.de>
2023-04-14 13:05:08 +0200

    update colors

--- src/main/java/com/ivision/engine/GameColor.java
+++ src/main/java/com/ivision/engine/GameColor.java
@@ -4,7 +4,7 @@ import javafx.scene.paint.Color;
 
 public interface GameColor{
 
-    Color VIOLETT = new Color(0.533,0.235,0.466,1);
+    Color VIOLETT = new Color(0.133,0.335,0.566,1);
     Color RED = new Color(0.988,0.266,0.392,1);
     Color BLUE = new Color(0.392,0.567,0.988,1);
     Color YELLOW = new Color(1.0,0.67,0.26,1);
